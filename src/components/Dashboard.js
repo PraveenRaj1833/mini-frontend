3afad04a3274285cc358b935196136a20a747b20
Grocerdevteam <developer.cybercode@gmail.com>
2021-05-03 17:59:20 +0530

    create test added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -110,7 +110,7 @@ export class Dashboard extends Component {
                             >
                             {/* <Button variant="secondary">Popover on {placement}</Button> */}
                             <button className="float-right mr-2 mt-1 inline btn btn-success border border-1" >
-                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" className="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                         </svg>{this.state.name}</button>
@@ -119,8 +119,8 @@ export class Dashboard extends Component {
     
     
                         <button className="float-right mr-2 mt-1 inline btn btn-success border border-1 green" onClick={this.logout}>Logout</button>
-                        <button class="small ui icon button float-right mr-2 btn-success notify mt-1 border border-1" width="20" height="20">
-                            <i class="bell icon " ></i>
+                        <button className="small ui icon button float-right mr-2 btn-success notify mt-1 border border-1" width="20" height="20">
+                            <i className="bell icon " ></i>
                         </button>
                         </span>:
                         <span>
